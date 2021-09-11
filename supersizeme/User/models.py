e81ae1ca53c590b081e0557702da70747da0a814
Victor Lieu <victorlieu@Victors-MacBook-Air.local>
2021-09-11 14:17:06 -0600

    Create user table and tentatitive views for them.

--- supersizeme/User/models.py
+++ supersizeme/User/models.py
@@ -1,12 +1,12 @@
-# models.py
 from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=60)
     age = models.CharField(max_length=60)
-    gender = models.IntegerField()
-    height = models.IntegerField() #centimeres
+    gender = models.IntegerField() # 0 for male, 1 for female, 2 for other
+    height = models.IntegerField() # centimeres
     weight = models.IntegerField() # Kgs
-    activityLevel = models.IntegerField() #enum
-    
+    activityLevel = models.IntegerField() # enum
+    #TODO: Last 5 Days of Logs
+
     def __str__(self):
-        return self.name
+        return self.username
