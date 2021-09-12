3d4e3d785606fc32401410259dd7f3ff5f264d4a
aplu@ualberta.ca <aplu@ualberta.ca>
2021-09-11 22:19:54 -0600

    settings.py in supersizeme subfolder changed

--- supersizeme/supersizeme/settings.py
+++ supersizeme/supersizeme/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'calrecs.apps.CalrecsConfig',
     'User.apps.UserConfig',
     'backend.apps.BackendConfig',
     'logs.apps.LogsConfig',
