b166a29850f797e50a1de6c528d6f1e33ff4ab96
Christopher Orlick <corlick@ualberta.ca>
2021-09-11 20:56:35 -0700

    heroku x4

--- supersizeme/supersizeme/settings.py
+++ supersizeme/supersizeme/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,5 @@ SESSION_COOKIE_SECURE = False
 ALLOWED_HOSTS=['*']
 2
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
